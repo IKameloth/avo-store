c39b7595a9ffe34d81837d4543553dcff31b4860
cmatteo <cmatteo@cmatteo-Latitude-7490>
2022-03-03 19:18:22 -0300

    getStaticProps with getStaticPaths

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,34 @@ import React, { useState, useEffect } from 'react'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
+import fetch from 'isomorphic-unfetch'
+// pagina estatica
+/*
+// ServerSideRendering
+export const getServerSideProps = async () => {
+  const response = await fetch('https://avo-store-vercel.vercel.app/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json() //rename data: productList
+      
+  return {
+    props: {
+      productList
+    }
+  }
+}*/
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([])
+// StaticSideGeneration
+export const getStaticProps = async () => {
+  const response = await fetch('https://avo-store-vercel.vercel.app/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json() //rename data: productList
+      
+  return {
+    props: {
+      productList
+    }
+  }
+}
 
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then((response) => response.json())
-      .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data)
-      })
-  }, [])
-
+const HomePage = ({productList}: {productList: TProduct[]}) => {
   return (
     <Layout>
       <KawaiiHeader />
